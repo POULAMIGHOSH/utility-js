89f8f89a14947da9273fdbd886db155e5775bcc3
poulami <poulami.21ghosh@gmail.com>
2021-08-29 16:16:26 +0530

    added the reduce funtion and passed all the tests and fixed one testcase error

---
+++ src/reduce.js
@@ -0,0 +1,38 @@
+const tail = require('./tail');
+const head = require('./head');
+
+const compute = (list, callBackFunction,result) => {
+    if(list.length >0 ){
+        r="";
+         return r+callBackFunction(head(list), compute(tail(list),callBackFunction,result));
+    }
+    return r;
+}
+
+const reduce = (list, callBackFunction,initialValue) =>
+{
+    let value
+    if (initialValue == undefined){
+    if (typeof (list[0]) == 'string') {
+        value = "";
+    }
+    else if (typeof(list[0]) == "number") {
+        value = 0;
+    }
+    else {
+        value = undefined;
+    }
+}
+else{
+    value = initialValue;     
+}
+if(list.length==0){
+    return value
+}
+
+var result = value+compute(list, callBackFunction,value);
+return result    
+   
+}
+
+module.exports = reduce;
